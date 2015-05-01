f707e0cdf13639f53be8438ca10fb3b58cf7fed5
Andrzej Pragacz <apragacz@o2.pl>
2015-05-01 19:47:02 +0200

    added add_command

--- jira_worklog/core.py
+++ jira_worklog/core.py
@@ -5,18 +5,28 @@ from .commands import day, today, yesterday
 from .exceptions import CommandError
 
 
+cmd_map = {}
+
+
+def add_command(cmd_module):
+    cmd_name = cmd_module.__name__.rsplit('.')[-1]
+    cmd_map[cmd_name] = cmd_module.command
+
+
+def default_command(*args):
+    print('invalid command', file=sys.stderr)
+
+
 def main(args):
+    add_command(day)
+    add_command(today)
+    add_command(yesterday)
+
+    cmd_name = args[1] if len(args) > 1 else None
+    cmd = cmd_map.get(cmd_name, default_command)
+    cmd_args = args[2:]
     try:
-        cmd = args[1] if len(args) > 1 else None
-        cmd_args = args[2:]
-        if cmd == 'day':
-            day.command(*cmd_args)
-        elif cmd == 'today':
-            today.command(*cmd_args)
-        elif cmd == 'yesterday':
-            yesterday.command(*cmd_args)
-        else:
-            print('invalid command', file=sys.stderr)
+        cmd(*cmd_args)
     except CommandError as exc:
         print(exc.args[0], file=sys.stderr)
 
