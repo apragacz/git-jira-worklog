fea57c21deab9888b22f6c5774f4850ec7528455
Andrzej Pragacz <apragacz@o2.pl>
2015-07-12 02:25:55 +0200

    added set_jira_issue

--- jira_worklog/git.py
+++ jira_worklog/git.py
@@ -28,12 +28,37 @@ def get_git_config(config_name):
     return data.decode('utf-8').rstrip()
 
 
+def set_git_config(config_name, value):
+    cmd = ['git', 'config', config_name, value]
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError:
+        raise CommandError('git config failed')
+
+
 def get_email():
     return get_git_config('user.email')
 
 
 def get_project_name():
     return get_git_config('jiraworklog.projectname')
+
+
+def get_current_branch():
+    cmd = ['git', 'branch']
+    try:
+        data = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError:
+        raise ValueError('git branch failed')
+    for line in data.decode('utf-8').split('\n'):
+        if line.startswith('*'):
+            return line[1:].strip()
+    raise ValueError('current branch not found')
+
+
+def set_jira_issue(issue):
+    branch = get_current_branch()
+    set_git_config('branch.{}.jiraissue'.format(branch), issue)
 
 
 def get_git_log_file():
