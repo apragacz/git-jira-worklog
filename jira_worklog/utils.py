3ba654b80c4f495ad163dc652181c2db15949ae4
Andrzej Pragacz <apragacz@o2.pl>
2015-12-05 02:52:41 +0100

    added merge_sorted

--- jira_worklog/utils.py
+++ jira_worklog/utils.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals, print_function
 import itertools
 import sys
+from collections import deque
 from functools import reduce
 
 
@@ -66,3 +67,60 @@ def next_pair(iterator):
         return (elem, True)
     except StopIteration:
         return (None, False)
+
+
+class ForwardSequence(object):
+
+    def __init__(self, iterable):
+        self.last_index = -1
+        self.last_value = None
+        self.iter = iter(iterable)
+
+    def __getitem__(self, key):
+        delta = key - self.last_index
+
+        if delta < 0:
+            raise ValueError('cannot access this index')
+
+        while delta:
+            try:
+                self.last_value = next(self.iter)
+                self.last_index += 1
+                delta -= 1
+            except StopIteration:
+                raise IndexError('index out of range')
+
+        return self.last_value
+
+
+def is_in_sequence(index, sequence):
+    try:
+        sequence[index]
+        return True
+    except IndexError:
+        return False
+
+
+def merge_sorted_sequences(seq1, seq2):
+    i = j = 0
+    while is_in_sequence(i, seq1) and is_in_sequence(j, seq2):
+        a = seq1[i]
+        b = seq2[j]
+        if a < b:
+            yield a
+            i += 1
+        else:
+            yield b
+            j += 1
+    while is_in_sequence(i, seq1):
+        yield seq1[i]
+        i += 1
+    while is_in_sequence(j, seq2):
+        yield seq2[j]
+        j += 1
+
+
+def merge_sorted(iterable1, iterable2):
+    for elem in merge_sorted_sequences(ForwardSequence(iterable1),
+                                       ForwardSequence(iterable2)):
+        yield elem
