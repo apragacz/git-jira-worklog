d970362784b5f529af05af3b0121a888483a9ce8
Andrzej Pragacz <apragacz@o2.pl>
2015-12-17 00:03:22 +0100

    refactor: added get_worklog_events

--- jira_worklog/commands/base.py
+++ jira_worklog/commands/base.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import, unicode_literals, print_function
 import datetime
 from functools import partial
 
+from ..config import get_repository_directories, retrieve_config
 from ..events import event_from_gitlog, group_events, pretty_form_tuple
 from ..exceptions import CommandError
 from ..git import get_email, get_git_log_file, get_project_name
 from ..utils.printing import bcolors, cprint
-from ..utils.functools import build_pipe
 from ..utils.compat import filter, map
+from ..utils.functools import build_pipe
+from ..utils.itertools import merge_sorted
 
 
 def get_last_weekday_date(weekday):
@@ -20,23 +22,36 @@ def get_last_weekday_date(weekday):
     return date
 
 
-def get_worklog_event_groups(date=None, team=None):
-    email = get_email()
-    project_name = get_project_name()
+def get_worklog_events(date, repo_path=None):
+    email = get_email(repo_path=repo_path)
+    project_name = get_project_name(repo_path=repo_path)
     if not email:
         raise CommandError('No email set')
     if not project_name:
         raise CommandError('No project name set')
 
-    p_out = get_git_log_file()
-    process_into_events = lambda project_name: build_pipe(
+    p_out = get_git_log_file(repo_path=repo_path)
+    process_into_events = build_pipe(
         partial(map, partial(event_from_gitlog, project_name=project_name)),
         partial(filter, lambda e: e.author_email == email),
         partial(filter, lambda e: e.datetime.date() == date),
         partial(sorted, key=lambda e: e.datetime),
     )
-    events = process_into_events(project_name)(p_out)
+    events = process_into_events(p_out)
     p_out.close()
+    return events
+
+
+def get_worklog_event_groups(date, team=None):
+    if team is not None:
+        config_data = retrieve_config()
+        repo_paths = get_repository_directories(config_data, team)
+        if not repo_paths:
+            raise CommandError('No repositories for team {}'.format(team))
+        events = merge_sorted(*[get_worklog_events(date, repo_path)
+                                for repo_path in repo_paths])
+    else:
+        events = get_worklog_events(date)
 
     events_groups = group_events(events)
     return events_groups
